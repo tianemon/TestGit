33bd04d4d23412e5edfc2d0abc7d5b2d18e85d4b
tianemon <811978297@qq.com>
2017-08-24 16:01:14 +0800

    idea commit

--- src/TestGit.java
+++ src/TestGit.java
@@ -2,4 +2,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public void test(){
+        System.out.println("haha");
+    }
 }
